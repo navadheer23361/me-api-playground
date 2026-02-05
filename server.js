987365480ed9927cb6504267a0a803af6126612d
Navadheer kumar vulli <vullinavadheerkumar4@gmail.com>
2026-02-05 14:51:02 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.listen(3000, ()=>{
+  console.log("Frontend running on http://localhost:3000");
+});
